6ab548ee80d8b671d30c195d493473f0848ab1ff
Michael Engelhard <engelhard.michael+git@gmail.com>
2016-03-11 16:18:08 +0100

    Added layout images. Changed folder structure

--- windows/create_ahk.py
+++ windows/create_ahk.py
@@ -1,5 +1,5 @@
+# coding: utf-8
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import csv
 
@@ -8,19 +8,19 @@ mappings = []
 # vk: Virtual Key
 # sc: Scan Code
 
-with open("mappings.csv", encoding='utf-8', newline='') as f:
+with open("../mappings.csv", encoding='utf-8', newline='') as f:
     for vk, sc, orig, new in csv.reader(f, delimiter=' ', quotechar=None):
         if sc == "sc" or not new:
             continue
         mappings.append({
-            "vk": vk, 
-            "sc":sc, 
-            "original_key": orig, 
+            "vk": vk,
+            "sc": sc,
+            "original_key": orig,
             "new_key": new,
             "new_ordinal": hex(ord(new))})
 
 # creation of akh file
-# sc02B: # key
+# sc02B is the scancode of the # key
 template = """
 Capslock & sc{sc}::
 sc02B & sc{sc}::
@@ -34,6 +34,6 @@ for mapping in mappings:
 
 PLACEHOLDER = "; >>> mappings go here <<<"
 
-with open("neolight.ahk", "w", encoding='utf-8') as f_out:
+with open("../build/neolight.ahk", "w", encoding='utf-8') as f_out:
     with open("template.ahk", "r", encoding='utf-8') as f_in:
         f_out.write(f_in.read().replace(PLACEHOLDER, result))
