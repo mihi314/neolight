68408bdef4cb5644aad75e9f33ee76b6f8b99c0a
Michael Engelhard <engelhard.michael+git@gmail.com>
2018-12-22 18:26:53 +0100

    Map + to Esc. Some refactoring of the ahk creation script

--- windows/create_ahk.py
+++ windows/create_ahk.py
@@ -1,32 +1,21 @@
-# coding: utf-8
 #!/usr/bin/env python3
 import csv
+import pandas as pd
 
 
-mappings = []
+mappings = pd.read_csv("mappings.csv", sep=" ", quoting=csv.QUOTE_NONE, encoding="utf-8")
+# exclude the ones that are empty
+mappings = mappings.loc[mappings.new.notna()]
 
-# vk: Virtual Key
-# sc: Scan Code
-with open("../mappings.csv", encoding='utf-8', newline='') as f:
-    for vk, sc, orig, new in csv.reader(f, delimiter=' ', quotechar=None):
-        if not new:
-            continue
-
-        # https://autohotkey.com/docs/commands/Send.htm
-        if len(new) == 1:
-            # single character
-            send_code = "{U+%s}" % hex(ord(new))
-        else:
-            # allows keys like Esc
-            send_code = "{%s}" % new
-
-        mappings.append({
-            "vk": vk,
-            "sc": sc,
-            "original_key": orig,
-            "new_key": new,
-            "send_code": send_code
-        })
+def generate_send_code(new):
+    # https://autohotkey.com/docs/commands/Send.htm
+    if len(new) == 1:
+        # single character
+        return "{U+%s}" % hex(ord(new))
+    else:
+        # allows keys like Esc
+        return "{%s}" % new
+mappings["send_code"] = mappings.new.apply(generate_send_code)
 
 # creation of the ahk file
 # sc02B is the scancode of the # key
@@ -37,12 +26,12 @@ sc02B & sc{sc}::
 return"""
 
 result = ""
-for mapping in mappings:
+for _, mapping in mappings.iterrows():
     result += template.format(**mapping)
 
 
 PLACEHOLDER = "; >>> mappings go here <<<"
 
-with open("../build/neolight.ahk", "w", encoding='utf-8') as f_out:
-    with open("template.ahk", "r", encoding='utf-8') as f_in:
+with open("build/neolight.ahk", "w", encoding='utf-8') as f_out:
+    with open("windows/template.ahk", "r", encoding='utf-8') as f_in:
         f_out.write(f_in.read().replace(PLACEHOLDER, result))
