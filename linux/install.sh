3f29a3b12e236db299c396e7fd8f9d0a03b41798
Michael Engelhard <engelhard.michael+git@gmail.com>
2020-03-10 22:28:48 +0100

    Add fourth layer. Use groups instead of modifiers for the different layers.

--- linux/install.sh
+++ linux/install.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname $0)
 install -m 644 "$BASEDIR/neolight" /usr/share/X11/xkb/symbols/neolight
-patch -r - /usr/share/X11/xkb/rules/evdev "$BASEDIR/evdev.patch"
+patch -N -r - /usr/share/X11/xkb/rules/evdev "$BASEDIR/evdev.patch"
