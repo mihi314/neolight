3413f20e4efa31cbab8eb0d72b107723ad067a62
Michael Engelhard <engelhard.michael+git@gmail.com>
2021-09-17 11:57:08 +0200

    Add esc variant to windows layout and remove dependency on pandas

--- windows/create_ahk.py
+++ windows/create_ahk.py
@@ -1,37 +1,74 @@
 #!/usr/bin/env python3
 import csv
-import pandas as pd
+import sys
+import os
+from textwrap import dedent
 
 
-mappings = pd.read_csv("mappings.csv", sep=" ", quoting=csv.QUOTE_NONE, encoding="utf-8")
-# exclude the ones that are empty
-mappings = mappings.loc[mappings.new.notna()]
-
-def generate_send_code(new):
+def generate_send_code(new_key: str) -> str:
     # https://autohotkey.com/docs/commands/Send.htm
-    if len(new) == 1:
+    if len(new_key) == 1:
         # single character
-        return "{U+%s}" % hex(ord(new))
+        return "{U+%s}" % hex(ord(new_key))
     else:
         # allows keys like Esc
-        return "{%s}" % new
-mappings["send_code"] = mappings.new.apply(generate_send_code)
-
-# creation of the ahk file
-# sc02B is the scancode of the # key
-template = """
-Capslock & sc{sc}::
-sc02B & sc{sc}::
-    send {send_code}
-return"""
-
-result = ""
-for _, mapping in mappings.iterrows():
-    result += template.format(**mapping)
+        return "{Blind}{%s}" % new_key
 
 
-PLACEHOLDER = "; >>> mappings go here <<<"
+def generate_ahk(esc_variant: bool) -> str:
+    with open("mappings.csv", encoding="utf-8") as f:
+        mappings = [row for row in csv.DictReader(f, delimiter=" ", quoting=csv.QUOTE_NONE)]
 
-with open("neolight.ahk", "w", encoding='utf-8') as f_out:
-    with open("template.ahk", "r", encoding='utf-8') as f_in:
-        f_out.write(f_in.read().replace(PLACEHOLDER, result))
+    ahk = ""
+
+    if esc_variant:
+        for mapping in mappings:
+            if mapping["sc"] == "010":
+                mapping["layer3"] = "Esc"
+
+        ahk += dedent(
+            """
+            ; map + key to Esc
+            sc01B::
+                send {Esc}
+            return"""
+        )
+
+    # sc02B is the scancode of the # key
+    template_layer3 = dedent(
+        """
+        CapsLock & sc{sc}::
+        sc02B & sc{sc}::
+            send {send_code}
+        return"""
+    )
+
+    # AppsKey is the Menu key
+    # sc056 is the scancode of the < key
+    template_layer4 = dedent(
+        """
+        AppsKey & sc{sc}::
+        sc056 & sc{sc}::
+            send {send_code}
+        return"""
+    )
+
+    for mapping in mappings:
+        if mapping["layer3"]:
+            ahk += template_layer3.format(sc=mapping["sc"], send_code=generate_send_code(mapping["layer3"]))
+        if mapping["layer4"]:
+            ahk += template_layer4.format(sc=mapping["sc"], send_code=generate_send_code(mapping["layer4"]))
+
+    PLACEHOLDER = "; >>> mappings go here <<<"
+
+    with open("template.ahk", "r", encoding="utf-8") as f:
+        return f.read().replace(PLACEHOLDER, ahk)
+
+
+os.makedirs("build", exist_ok=True)
+
+with open("build/neolight.ahk", "w", encoding="utf-8") as f:
+    f.write(generate_ahk(esc_variant=False))
+
+with open("build/neolight-esc.ahk", "w", encoding="utf-8") as f:
+    f.write(generate_ahk(esc_variant=True))
